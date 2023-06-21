c22b2777e1fe734a2ce40e349eba0b1f83b95d93
Fitra Aziz Al Rasyid <fitradevelopers71@gmail.com>
2023-06-22 00:44:30 +0700

    wss

--- client/constants/index.ts
+++ client/constants/index.ts
@@ -1,2 +1,2 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://chat.fitradev.my.id'
-export const WEBSOCKET_URL = process.env.NEXT_PUBLIC_WEBSOCKET_URL || 'ws://chat.fitradev.my.id'
+export const WEBSOCKET_URL = process.env.NEXT_PUBLIC_WEBSOCKET_URL || 'wss://chat.fitradev.my.id'
