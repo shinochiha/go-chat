7485348f5de0ecdc25c784aaaed7445f0623a8a2
Fitra Aziz Al Rasyid <fitradevelopers71@gmail.com>
2023-06-22 00:40:08 +0700

    change to https://chat.fitradev.my.id

--- client/constants/index.ts
+++ client/constants/index.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:9090'
-export const WEBSOCKET_URL = process.env.NEXT_PUBLIC_WEBSOCKET_URL || 'ws://127.0.0.1:9090'
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://chat.fitradev.my.id'
+export const WEBSOCKET_URL = process.env.NEXT_PUBLIC_WEBSOCKET_URL || 'ws://chat.fitradev.my.id'
